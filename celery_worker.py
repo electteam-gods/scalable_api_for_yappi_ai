09c6b8156cbed48eed479997084ac57cb3ed9ff7
Айяр Халилов <super.ayar-04@yandex.ru>
2024-09-29 07:39:01 +0300

    better ai response

--- celery_worker.py
+++ celery_worker.py
@@ -24,5 +24,5 @@ def find_duplicates(url: str):
     print('RESULT', result)
     return FindDuplicatesTaskResult(
         is_duplicate=result['answer'],
-        duplicate_for=result['id']['0'] if result['answer'] else None
+        duplicate_for=result['id'] if result['answer'] else None
     ).model_dump(mode='json', exclude_none=True)
